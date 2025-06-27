2ba778e84f68ffd337aff45e2ed81803801cfce8
xX2bXx <188763131+xX2bXx@users.noreply.github.com>
2025-06-27 14:06:56 +0300

    feat: add fallback creation and argument handling improvements

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 )
 
@@ -16,25 +17,23 @@ func main() {
 	}
 
 	hasBackup := true
-	if getIndexOfBackup() == 0 && len(os.Args) == 3 {
+	if getIndexOf("--backup") == 0 && (len(os.Args) == 3 || len(os.Args) == 5) {
 		hasBackup = false
-	} else if len(os.Args) != 5 {
-		fmt.Printf("usage: \"zbm-update [--target /path/to/ZBM] [--backup /path/to/backup (if you have one)]\"\n")
+	} else if len(os.Args) != 7 {
+		fmt.Printf("usage: \"zbm-update --target /path/to/ZBM.EFI [--backup /path/to/BACKUP.EFI (if you have one)] [--fallback true|false]\"\n")
 		return
 	}
 	
-	targetIndex := getIndexOfTarget()
-	if targetIndex == 0 {
+	if getIndexOf("--target") == 0 {
 		log.Fatalf("ERROR: --target argument not found\n")
 	}
 
-	targetPath := os.Args[targetIndex]
-	if !filepath.IsAbs(targetPath) {
+	if !filepath.IsAbs(os.Args[getIndexOf("--target")]) {
 		log.Fatal("ERROR: Target path must be absolute\n")
 	}
 
+	backupPath := os.Args[getIndexOf("--backup")]
 	if hasBackup {
-		backupPath := os.Args[getIndexOfBackup()]
 		log.Printf("Creating backup file... ")
 		if err := createBackup(targetPath, backupPath); err != nil {
 			log.Fatalf("ERROR: %v\n", err)
@@ -46,7 +45,18 @@ func main() {
 	if err := installNewBootloader(targetPath); err != nil {
 		log.Fatalf("ERROR: %v\n", err)
 	}
-	log.Printf("Success!\nNo errors reported! Enjoy your updated ZFSBootMenu :)")
+	log.Printf("Success!\n")
+
+	if value, err := strconv.ParseBool(os.Args[getIndexOf("--fallback")]); err != nil {
+		log.Fatalf("ERROR: %v\n", err)
+	} else if value {
+		log.Printf("Creating fallback (/boot/efi/EFI/BOOT/BOOTX64.EFI)...")
+		if err := createFallback(hasBackup, targetPath, backupPath); err != nil {
+			log.Fatalf("ERROR: %v\n", err)
+		}	
+		log.Printf("Success!\n")
+	}
+	log.Printf("No errors reported! Enjoy your updated ZFSBootMenu :)\n")
 }
 
 func createBackup(targetPath, backupPath string) error {
@@ -54,10 +64,8 @@ func createBackup(targetPath, backupPath string) error {
 	if err != nil {
 		return err
 	}
-	if err := os.WriteFile(backupPath, data, 0600); err != nil {
-		return err
-	}
-	return nil
+	
+	return os.WriteFile(backupPath, data, 0600)
 }
 
 func installNewBootloader(targetPath string) error {
@@ -74,12 +82,16 @@ func installNewBootloader(targetPath string) error {
 		return fmt.Errorf("Download failed: HTTP %d", resp.StatusCode)
 	}
 
-	tmpFile := targetPath + ".tmp"
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
+	if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
+		return err
+	}
+
+	tmpFile := targetPath + ".tmp"
 	if err := os.WriteFile(tmpFile, data, 0644); err != nil {
 		return err
 	}
@@ -92,18 +104,29 @@ func installNewBootloader(targetPath string) error {
 	return nil
 }
 
-func getIndexOfTarget() int {
-	for index := range os.Args {
-		if os.Args[index] == "--target" && len(os.Args) > index+1 {
-			return index + 1
-		}
+func createFallback(hasBackup bool, targetPath, backupPath string) error {
+	var data []byte
+	var err error
+	
+	if hasBackup {
+		data, err = os.ReadFile(backupPath)
+	} else {
+		data, err = os.ReadFile(targetPath)
 	}
-	return 0
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll("/boot/efi/EFI/BOOT", 0755); err != nil {
+		return err
+	}
+
+	return os.WriteFile("/boot/efi/EFI/BOOT/BOOTX64.EFI", data, 0644)
 }
 
-func getIndexOfBackup() int {
+func getIndexOf(flag string) int {
 	for index := range os.Args {
-		if os.Args[index] == "--backup" && len(os.Args) > index+1 {
+		if os.Args[index] == flag && len(os.Args) > index+1 {
 			return index + 1
 		}
 	}
